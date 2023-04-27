647895058039fa16bef83c7ca9d334abca830bdb
Vitaliy <bezuglivita123@gmail.com>
2023-04-27 18:15:21 +0300

    v1

---
+++ WeatherForecast.Api/Controllers/HealthCheck.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecast.Api.Controllers;
+
+[Route("")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult HealthCheck()
+    {
+        return Ok("Service are online!");
+    }
+}
